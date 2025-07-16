90260b7560523585f479cb678839e1c0d3b73bde
dursunemre <dursunemredurhan@hotmail.com>
2025-07-16 13:57:34 +0300

    Tüm dosyalar ilk kez eklendi

---
+++ client/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Akıllı Fit App'e Hoş Geldin!
+      </h1>
+    </div>
+  );
+}
