fd767f82a171b4baecb9f4ad3b31eb72f55dbf96
dursunemre <dursunemredurhan@hotmail.com>
2025-07-16 16:57:04 +0300

    kullanıcı sınıfı bitti

--- server/AkilliFitApp.API/Entities/Kullanici.cs
+++ server/AkilliFitApp.API/Entities/Kullanici.cs
@@ -1,6 +1,17 @@
-﻿namespace AkilliFitApp.API.Entities
+﻿
+
+namespace AkilliFitApp.API.Entities
 {
     public class Kullanici
     {
+        public required string Uid { get; set; }
+        public required string Ad { get; set; }
+        public required string Soyad { get; set; }
+        public DateOnly DogumTarihi { get; set; }
+        public required string Cinsiyet { get; set; }
+        public float Kilo { get; set; }
+        public float HedefKilo { get; set; }
+        public int AktiviteSeviyesi { get; set; }
+        public required string ProfilResmiUrl { get; set; }
     }
 }
