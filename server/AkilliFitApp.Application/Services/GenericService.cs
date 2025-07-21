d1cf97eca741b6d945501cb18b8fccc28ed29abe
dursunemre <dursunemredurhan@hotmail.com>
2025-07-21 16:12:58 +0300

    Repo katmanı kaldırıldı

---
+++ server/AkilliFitApp.Application/Services/GenericService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AkilliFitApp.Domain.Interfaces;
+
+namespace AkilliFitApp.Application.Services
+{
+    internal class GenericService : IService
+    {
+
+    }
+}
---
+++ server/AkilliFitApp.Domain/Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliFitApp.Domain.Interfaces
+{
+    public interface IService
+    {
+        Task<IEnumerable<T>> GetAllAsync<T>() where T : class;
+    }
+}
