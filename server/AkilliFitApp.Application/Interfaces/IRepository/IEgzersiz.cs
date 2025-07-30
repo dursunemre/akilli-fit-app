c5e3ce6f2dd3efdd798bb113b672761092b12963
dursunemre <dursunemredurhan@hotmail.com>
2025-07-30 14:27:53 +0300

    egzersiz repo, servis ve controller eklendi

---
+++ server/AkilliFitApp.Application/Interfaces/IRepository/IEgzersiz.cs
@@ -0,0 +1,15 @@
+﻿using AkilliFitApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliFitApp.Application.Interfaces.IRepository
+{
+    public interface IEgzersizRepository
+    {
+        Task<IEnumerable<Egzersiz>> GetAllByTipAsync(string tip);
+    }
+
+}
---
+++ server/AkilliFitApp.Application/Interfaces/IService/IEgzersizService.cs
@@ -0,0 +1,15 @@
+﻿using AkilliFitApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliFitApp.Application.Interfaces.IService
+{
+    public interface IEgzersizService
+    {
+        Task<IEnumerable<Egzersiz>> GetAllByTipAsync(string tip);
+    }
+
+}
